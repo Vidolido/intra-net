699a79343c50aa1e4d51907bb50f0260e22f42bc
Goce Levkovski <goceto@hotmail.com>
2024-06-14 20:59:48 +0200

    od lap top

--- src/components/settings/insertSettingsForm/DisplayCollections.jsx
+++ src/components/settings/insertSettingsForm/DisplayCollections.jsx
@@ -3,62 +3,73 @@
 // state/context
 import { REMOVE_FROM_COLLECTION } from '@/state/actionTypes';
 import {
-	useSettingsContext,
-	useSettingsDispatchContext,
+  useSettingsContext,
+  useSettingsDispatchContext,
 } from '@/state/settingsContext';
 
 // utils
-import { generateUUID } from '@/utils/generateUUID';
 
 // components
 import ContextButton from '@/components/buttons/ContextButton';
 
 const DisplayCollections = ({ languages, defaultLanguage }) => {
-	const dispatch = useSettingsDispatchContext();
-	const { selectedCollection, optionsSchema } = useSettingsContext();
-	const { collections } = optionsSchema;
+  const dispatch = useSettingsDispatchContext();
+  const { selectedCollection, optionsSchema } = useSettingsContext();
+  const { collections } = optionsSchema;
 
-	const handleDelete = (item) => {
-		dispatch({
-			type: REMOVE_FROM_COLLECTION,
-			payload: {
-				state: 'optionsSchema',
-				value: item,
-				more: {
-					property: 'collections',
-				},
-			},
-		});
-	};
+  const handleDelete = (item) => {
+    dispatch({
+      type: REMOVE_FROM_COLLECTION,
+      payload: {
+        state: 'optionsSchema',
+        value: item,
+        more: {
+          property: 'collections',
+        },
+      },
+    });
+  };
 
-	return (
-		<fieldset name='collection-items'>
-			<ul className='pl-5 flex flex-col gap-1'>
-				{collections &&
-					collections[selectedCollection]?.items?.map((item) => {
-						return (
-							<li
-								className='list-disc border border-slate-50 hover:border-red-200 focus:outline-none'
-								key={item.id}>
-								<div className='flex justify-between gap-2'>
-									<span className='block border-l border-slate-300 px-2'>
-										{(typeof item.value === 'string' && item.value) ||
-											item.value[defaultLanguage.language] ||
-											`${item?.value?.key} - ${item?.value?.value}`}
-									</span>
-									<ContextButton
-										label='delete'
-										type='default'
-										onClick={() => handleDelete(item.id)}
-										classes='border-l border-slate-300 px-2'
-									/>
-								</div>
-							</li>
-						);
-					})}
-			</ul>
-		</fieldset>
-	);
+  const handleEdit = (_id) => {
+    console.log(_id);
+  };
+
+  return (
+    <fieldset name='collection-items'>
+      <ul className='pl-5 flex flex-col gap-1'>
+        {collections &&
+          collections[selectedCollection]?.items?.map((item) => {
+            return (
+              <li
+                className='list-disc border border-slate-50 hover:border-red-200 focus:outline-none'
+                key={item.id}>
+                <div className='flex justify-between gap-2'>
+                  <span className='block border-l border-slate-300 px-2'>
+                    {(typeof item.value === 'string' && item.value) ||
+                      item.value[defaultLanguage.language] ||
+                      `${item?.value?.key} - ${item?.value?.value}`}
+                  </span>
+                  <div>
+                    <ContextButton
+                      label='edit'
+                      type='default'
+                      onClick={() => handleEdit(item.id)}
+                      classes='border-l border-slate-300 px-2'
+                    />
+                    <ContextButton
+                      label='delete'
+                      type='default'
+                      onClick={() => handleDelete(item.id)}
+                      classes='border-l border-slate-300 px-2'
+                    />
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+      </ul>
+    </fieldset>
+  );
 };
 
 export default DisplayCollections;
