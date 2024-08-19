4966092accb94707d800d6b3c551e9520d1c91c3
Goce Levkovski <goceto@hotmail.com>
2024-08-19 15:36:04 +0200

    small css changes

--- src/components/analyses/Analysis/IdentificationNumbers/index.jsx
+++ src/components/analyses/Analysis/IdentificationNumbers/index.jsx
@@ -10,51 +10,56 @@ import InputType from '@/components/inputs/InputType';
 import SelectInput from '@/components/inputs/SelectInput';
 
 const IdentificationNumbers = ({
-	languages,
-	analysis,
-	identificationNumbers,
+  languages,
+  analysis,
+  identificationNumbers,
 }) => {
-	let names = identificationNumbers?.settings.map((setting) => ({
-		_id: setting._id,
-		...nameArray(setting.parameter.inputValue),
-		value: '',
-	}));
-	const [state, setState] = useState(names);
-	const [selected, setSelected] = useState(names[0]._id);
+  let names = identificationNumbers?.settings.map((setting) => ({
+    _id: setting._id,
+    ...nameArray(setting.parameter.inputValue),
+    value: '',
+  }));
+  const [state, setState] = useState(
+    !analysis.identificationNumbers.length
+      ? names
+      : analysis.identificationNumbers
+  );
+  const [selected, setSelected] = useState(names[0]._id);
 
-	const handleChange = (e) => {
-		const newState = [...state];
-		let selection = newState.find((item) => item._id.toString() === selected);
-		selection.value = e.target.value;
-		setState(newState);
-	};
+  const handleChange = (e) => {
+    const newState = [...state];
+    let selection = newState.find((item) => item._id.toString() === selected);
+    selection.value = e.target.value;
+    setState(newState);
+  };
 
-	const handleBlur = async () => {
-		await saveIdentificationNumbers(state, analysis._id);
-	};
+  const handleBlur = async () => {
+    await saveIdentificationNumbers(state, analysis._id);
+  };
 
-	const handleOnChange = (e) => {
-		setSelected(e.target.value);
-	};
-	return (
-		<form className='p-1 border border-slate-200 rounded'>
-			<h6>Identification Numbers</h6>
-			<div className='flex w-fit gap-2'>
-				<InputType
-					onBlur={handleBlur}
-					onChange={handleChange}
-					classes={'w-28'}
-					value={state.find((item) => item._id.toString() === selected).value}
-				/>
-				<SelectInput
-					options={state}
-					property={'name'}
-					defaultLanguage={'en'}
-					onChange={handleOnChange}
-				/>
-			</div>
-		</form>
-	);
+  const handleOnChange = (e) => {
+    setSelected(e.target.value);
+  };
+
+  return (
+    <form className='pb-1'>
+      <h6>Identification Numbers</h6>
+      <div className='flex w-fit gap-2'>
+        <InputType
+          onBlur={handleBlur}
+          onChange={handleChange}
+          classes={'w-28'}
+          value={state.find((item) => item._id.toString() === selected).value}
+        />
+        <SelectInput
+          options={state}
+          property={'name'}
+          defaultLanguage={'en'}
+          onChange={handleOnChange}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default IdentificationNumbers;
--- src/components/analyses/Analysis/index.jsx
+++ src/components/analyses/Analysis/index.jsx
@@ -8,48 +8,50 @@ import TemplateForm from '../analysesTemplate/TemplateForm';
 import IdentificationNumbers from './IdentificationNumbers';
 
 const Analysis = ({
-	analysis,
-	templateSettings,
-	languages,
-	settings,
-	templates,
+  analysis,
+  templateSettings,
+  languages,
+  settings,
+  templates,
 }) => {
-	let { products, types, countries, fields, identificationNumbers } =
-		mutateTemplateSettings(templateSettings);
-	// console.log(analysis, 'the analysis');
-	const template = templates.find(
-		(template) => template._id === analysis.templateId
-	);
-	const analysisTemplate = !analysis.template ? null : analysis.template;
-	// console.log(analysisTemplate, 'analisysTemplate');
+  let { products, types, countries, fields, identificationNumbers } =
+    mutateTemplateSettings(templateSettings);
+  // console.log(analysis, 'the analysis');
+  const template = templates.find(
+    (template) => template._id === analysis.templateId
+  );
+  const analysisTemplate = !analysis.template ? null : analysis.template;
+  // console.log(analysisTemplate, 'analisysTemplate');
 
-	return (
-		<div className='flex gap-2'>
-			<div className='flex flex-col gap-1'>
-				<IdentificationNumbers
-					languages={languages}
-					analysis={analysis}
-					identificationNumbers={identificationNumbers}
-				/>
-				<SelectFields fields={fields.settings} analysis={analysis} />
-				<TemplateSelectForm
-					analysis={analysis}
-					languages={languages}
-					products={products}
-					types={types}
-					countries={countries}
-					templates={templates}
-				/>
-			</div>
-			<TemplateForm
-				templateId={analysis.templateId}
-				template={analysisTemplate || template?.template}
-				languages={languages}
-				settings={settings}
-				analysisId={analysis._id}
-			/>
-		</div>
-	);
+  return (
+    <div className='flex gap-6 pr-3'>
+      <div className='flex flex-col gap-1 shrink'>
+        <IdentificationNumbers
+          languages={languages}
+          analysis={analysis}
+          identificationNumbers={identificationNumbers}
+        />
+        <SelectFields fields={fields.settings} analysis={analysis} />
+        <TemplateSelectForm
+          analysis={analysis}
+          languages={languages}
+          products={products}
+          types={types}
+          countries={countries}
+          templates={templates}
+        />
+      </div>
+      <div className='w-[60%]'>
+        <TemplateForm
+          templateId={analysis.templateId}
+          template={analysisTemplate || template?.template}
+          languages={languages}
+          settings={settings}
+          analysisId={analysis._id}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Analysis;
