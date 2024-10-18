d5f5a4fc0f56fb2432d49ac446b01689fc30b3bf
vidothedesigner <48659615+vidothedesigner@users.noreply.github.com>
2024-10-18 19:35:11 +0200

    HeaderForm done

--- src/components/reusable/SelectInput/index.jsx
+++ src/components/reusable/SelectInput/index.jsx
@@ -1,54 +1,54 @@
 import { useEffect, useState } from 'react';
 
 // helper
-const initializeSelectState = (defaultLanguage, data) => {
-  return data.state.map((option) => ({
-    _id: option._id,
-    value: option.name[defaultLanguage],
-  }));
-};
+const initializeSelectState = (defaultLanguage, data) =>
+	data.state.map(({ _id, name }) => ({
+		...(_id && { _id }),
+		value: name[defaultLanguage],
+	}));
 
 const SelectInput = ({
-  defaultLanguage,
-  data = null,
-  extractData = null,
-  showEmptyOption,
+	defaultLanguage,
+	data = null,
+	extractData = null,
+	showEmptyOption,
 }) => {
-  const [state, setState] = useState(() =>
-    initializeSelectState(defaultLanguage, data)
-  );
-  const [selected, setSelected] = useState(data.defaultValue || state[0]._id);
+	console.log(data, 'the data');
+	const [state, setState] = useState(() =>
+		initializeSelectState(defaultLanguage, data)
+	);
+	const [selected, setSelected] = useState(data.defaultValue || state[0]._id);
 
-  useEffect(() => {
-    const newState = initializeSelectState(defaultLanguage, data);
-    setState(newState);
-  }, [data, defaultLanguage]);
+	useEffect(() => {
+		const newState = initializeSelectState(defaultLanguage, data);
+		setState(newState);
+	}, [data, defaultLanguage]);
 
-  const onSelectChange = (e) => {
-    const { value } = e.target;
-    setSelected(value);
-    extractData !== null && extractData(state);
-  };
-
-  return (
-    <fieldset className={data?.classes}>
-      <label>{data?.label}</label>
-      <select
-        name={data?.selectName}
-        className={`box-content border border-grey-50 border-opacity-60 rounded hover:border-red-200 focus:outline-none cursor-pointer ${data?.classes}`}
-        onChange={onSelectChange}
-        value={selected}>
-        {showEmptyOption && <option value=''>--</option>}
-        {state?.map((option, index) => (
-          <option
-            key={option.id || option._id || index}
-            value={option.id || option._id || option.value}>
-            {option.value}
-          </option>
-        ))}
-      </select>
-    </fieldset>
-  );
+	const onSelectChange = (e) => {
+		const { value } = e.target;
+		setSelected(value);
+		extractData !== null && extractData(state);
+	};
+	console.log(state, 'ovoa');
+	return (
+		<fieldset className={data?.classes}>
+			<label>{data?.label}</label>
+			<select
+				name={data?.selectName}
+				className={`box-content border border-grey-50 border-opacity-60 rounded hover:border-red-200 focus:outline-none cursor-pointer ${data?.classes}`}
+				onChange={onSelectChange}
+				value={selected}>
+				{showEmptyOption && <option value=''>--</option>}
+				{state?.map((option, index) => (
+					<option
+						key={option.id || option._id || index}
+						value={option.id || option._id || option.value}>
+						{option.value}
+					</option>
+				))}
+			</select>
+		</fieldset>
+	);
 };
 
 export default SelectInput;
--- src/components/settings/Setting/HeaderForm/index.jsx
+++ src/components/settings/Setting/HeaderForm/index.jsx
@@ -4,7 +4,6 @@ import { useFormState } from 'react-dom';
 
 // state/actions
 import { saveSettingHeader } from '@/serverActions/settings/saveSettingHeader';
-import { generateUUID } from '@/utils/generateUUID';
 
 // components
 import ArrowSvg from '@/../public/arrow.svg';
@@ -13,104 +12,101 @@ import NormalInput from '@/components/reusable/NormalInput';
 import ContextButton from '@/components/buttons/ContextButton';
 
 const status = [
-  {
-    id: generateUUID(),
-    name: {
-      en: 'draft',
-      mk: 'драфт',
-      gr: 'гр',
-    },
-  },
-  {
-    id: generateUUID(),
-    status: 'published',
-    name: {
-      en: 'published',
-      mk: 'објавен',
-      gr: 'гр',
-    },
-  },
+	{
+		name: {
+			en: 'draft',
+			mk: 'драфт',
+			gr: 'гр',
+		},
+	},
+	{
+		name: {
+			en: 'published',
+			mk: 'објавен',
+			gr: 'гр',
+		},
+	},
 ];
 
 const HeaderForm = ({ languages, sectors, setting }) => {
-  const [state, formAction] = useFormState(saveSettingHeader, {
-    message: '',
-    error: '',
-  });
+	const [state, formAction] = useFormState(saveSettingHeader, {
+		message: '',
+		error: '',
+	});
 
-  let hasName = !setting.settingName;
-  const [visible, setVisible] = useState(hasName);
+	let hasName = !setting.settingName;
+	const [visible, setVisible] = useState(hasName);
 
-  return (
-    <form action={formAction} className='p-1 border border-slate-200 rounded'>
-      <input
-        type='text'
-        className='hidden'
-        defaultValue={setting._id}
-        name='document_id'
-      />
-      <button
-        type='button'
-        onClick={() => setVisible(!visible)}
-        className='relative w-full'>
-        <h4 className='text-left'>Document Settings</h4>
-        <ArrowSvg
-          className={`w-[22px] h-[22px] absolute right-1 top-[3px] fill-red-500 hover:fill-red-300 ${
-            visible ? '' : 'rotate-180'
-          }`}
-        />
-      </button>
-      <div
-        className={`flex w-full gap-[1px] bg-slate-100 border-[1px] border-slate-100 p-[1px] rounded ${
-          !visible ? 'hidden' : 'visible'
-        }`}>
-        <SelectInput
-          defaultLanguage={languages[0].language}
-          data={{
-            state: sectors,
-            // defaultValue: setting.sector,
-            label: 'Sector',
-            selectName: 'sector',
-            showEmptyOption: false,
-            classes: 'flex flex-col items-start bg-white px-[2px]',
-          }}
-        />
-        <NormalInput
-          data={{
-            state: setting.settingName,
-            name: 'settingName',
-            label: 'Setting Name',
-            fieldsetClass: 'flex flex-col grow bg-white px-[2px]',
-            inputClass: 'h-21px',
-            required: true,
-          }}
-        />
+	return (
+		<form action={formAction} className='p-1 border border-slate-200 rounded'>
+			<input
+				type='text'
+				className='hidden'
+				defaultValue={setting._id}
+				name='document_id'
+			/>
+			<button
+				type='button'
+				onClick={() => setVisible(!visible)}
+				className='relative w-full'>
+				<h4 className='text-left'>Document Settings</h4>
+				<ArrowSvg
+					className={`w-[22px] h-[22px] absolute right-1 top-[3px] fill-red-500 hover:fill-red-300 ${
+						visible ? '' : 'rotate-180'
+					}`}
+				/>
+			</button>
+			<div
+				className={`flex w-full gap-[1px] bg-slate-100 border-[1px] border-slate-100 p-[1px] rounded ${
+					!visible ? 'hidden' : 'visible'
+				}`}>
+				<SelectInput
+					defaultLanguage={languages[0].language}
+					data={{
+						state: sectors,
+						defaultValue: setting.sector,
+						label: 'Sector',
+						selectName: 'sector',
+						showEmptyOption: false,
+						classes: 'flex flex-col items-start bg-white px-[2px]',
+					}}
+				/>
+				<NormalInput
+					data={{
+						state: setting.settingName,
+						name: 'settingName',
+						label: 'Setting Name',
+						fieldsetClass: 'flex flex-col grow bg-white px-[2px]',
+						inputClass: 'h-21px',
+						required: true,
+					}}
+				/>
 
-        {setting.settingName != null && (
-          <SelectInput
-            defaultLanguage={languages[0].language}
-            data={{
-              state: status,
-              defaultValue: setting.status,
-              label: 'Status',
-              selectName: 'status',
-              showEmptyOption: false,
-              classes: 'flex flex-col items-start bg-white px-[2px]',
-            }}
-          />
-        )}
-      </div>
-      {state?.error && <p>{state?.error}</p>}
-      {visible && (
-        <ContextButton
-          label='Save Document Settings'
-          type='edit'
-          onClick={(e) => e.target.form.requestSubmit()}
-          classes='w-full mt-[2px]'
-        />
-      )}
-    </form>
-  );
+				{setting.settingName != null && (
+					<SelectInput
+						defaultLanguage={languages[0].language}
+						data={{
+							state: status,
+							defaultValue: setting.documentStatus,
+							label: 'Status',
+							selectName: 'status',
+							showEmptyOption: false,
+							classes: 'flex flex-col items-start bg-white px-[2px]',
+						}}
+					/>
+				)}
+			</div>
+			{state?.error && <p>{state?.error}</p>}
+			{visible && (
+				<ContextButton
+					label='Save Document Settings'
+					type='edit'
+					onClick={(e) => e.target.form.requestSubmit()}
+					classes='w-full mt-[2px]'
+				/>
+			)}
+		</form>
+	);
 };
 
 export default HeaderForm;
